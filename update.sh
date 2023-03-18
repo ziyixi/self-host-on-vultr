0b0f6bd6ef96c5338906beb4b6a2148581d24bb3
Ziyi Xi <ziyixi@mail.ustc.edu.cn>
2023-03-18 14:32:53 -0400

    update script

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Stopping containers..."
 docker compose down
+echo "Pulling latest images..."
 docker compose pull
+echo "Removing old images..."
 docker image prune -f
+echo "Starting containers..."
 docker compose up -d
